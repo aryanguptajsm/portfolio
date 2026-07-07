62c9420b5bb0434f97659752e7441d6cd7cdd4d8
Aryan gupta <aryangupta.jsm@gmail.com>
2026-07-07 23:51:12 +0530

    feat: implement custom smooth-following cursor component in App

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,7 @@ import Projects from './components/Projects';
 import Experience from './components/Experience';
 import Contact from './components/Contact';
 import Footer from './components/Footer';
-import SmoothCursor from './components/smoothcursor';
+import { SmoothCursor } from '@/components/ui/smooth-cursor';
 import './index.css';
 
 function App() {
